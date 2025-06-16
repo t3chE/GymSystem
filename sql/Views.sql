c2f36f81a9636748587153c751fdefb028e76ed9
t3chE <t0dtym3@gmail.com>
2025-06-16 14:29:03 +0100

    Views file created

---
+++ sql/Views.sql
@@ -0,0 +1,8 @@
+-- Use the GymDB database
+USE GymDB;
+
+-- ==============================================================================
+-- View: ActiveMembers
+-- Description: Shows details of all members whose membership is currently active
+--              (MembershipEndDate is today or in the future).
+-- ==============================================================================
