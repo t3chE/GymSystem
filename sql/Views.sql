4037778e8f2eaf44fe6a507b5c64121ddba95aaa
t3chE <t0dtym3@gmail.com>
2025-06-16 14:30:23 +0100

    ActiveMembers view added

--- sql/Views.sql
+++ sql/Views.sql
@@ -6,3 +6,22 @@ USE GymDB;
 -- Description: Shows details of all members whose membership is currently active
 --              (MembershipEndDate is today or in the future).
 -- ==============================================================================
+DROP VIEW IF EXISTS ActiveMembers;
+CREATE VIEW ActiveMembers AS
+SELECT
+    M.MemberID,
+    M.FirstName,
+    M.LastName,
+    M.Email,
+    M.Phone,
+    MP.PlanName,
+    MP.Price AS CurrentPlanPrice,
+    M.MembershipStartDate,
+    M.MembershipEndDate,
+    M.OustandingBalance
+FROM
+    Members M
+JOIN
+    MembershipPlans MP ON M.PlanID = MP.PlanID
+WHERE
+    M.MembershipEndDate >= CURRENT_DATE();
